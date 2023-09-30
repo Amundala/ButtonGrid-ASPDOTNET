167c355d2eedc670876dd523ced5d7dbdc44a806
Tom Amundala <100129520+toma2k@users.noreply.github.com>
2023-09-30 10:37:46 +0300

    First Commit

---
+++ ButtonGrid/Controllers/Button.cs
@@ -0,0 +1,105 @@
+﻿using ButtonGrid.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Globalization;
+
+namespace ButtonGrid.Controllers
+{
+    public class Button : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        Random random = new Random();
+        private NumberStyles style;
+        const int GRID_SIZE = 25;
+
+        public IActionResult Index()
+        {
+            if(buttons.Count < GRID_SIZE)
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                            {
+                                buttons.Add(new ButtonModel { Id = i, ButtonState = random.Next(4) });
+                            }
+            }
+            
+            
+            return View("Index", buttons);
+        }
+        public IActionResult HandleButtonClick( string buttonNumber)
+        {
+            int bn = int.Parse(buttonNumber);
+
+            buttons.ElementAt(bn).ButtonState = (buttons.ElementAt(bn).ButtonState + 1) % 4;
+            
+            return View("Index", buttons);
+        }
+
+        [HttpPost]
+        public IActionResult ShowOneButton(int buttonNumber)
+        {
+            // add one to the button state. if > 4, then reset to zero
+            buttons.ElementAt(buttonNumber).ButtonState = (buttons.ElementAt(buttonNumber).ButtonState + 1) % 4;
+
+            // Render a button and Save it to a string
+            string buttonString = RenderRazorViewToString(this, "ShowOneButton", buttons.ElementAt(buttonNumber));
+
+            //Generate a win or loss string based on the state of the button array
+            bool DidIWinYet = true;
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if(buttons.ElementAt(i).ButtonState != buttons.ElementAt(0).ButtonState)
+              
+                    DidIWinYet = false;
+            }
+            string messageString = "";
+            if (DidIWinYet == true)
+                messageString = "<p>Hurray! You Won They're all A Match</p>";
+            else
+                messageString = "<p>Oops! Waraburije</p>";
+
+            // Assembly a JSON string that has two parts (button string htlm and win loss message)
+            var package = new { part1 = buttonString, part2 = messageString };
+            // Send JSON result
+            return Json(package);
+
+            //return PartialView(buttons.ElementAt(buttonNumber));
+        }
+        public static string RenderRazorViewToString(Controller controller, string viewName, object model = null)
+        {
+            controller.ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                IViewEngine? viewEngine =
+                    controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as
+                        ICompositeViewEngine;
+
+                ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, false);
+
+                ViewContext viewContext = new ViewContext(
+                    controller.ControllerContext,
+                    viewResult.View,
+                    controller.ViewData,
+                    controller.TempData,
+                    sw,
+                    new HtmlHelperOptions()
+                );
+                if (viewResult.View != null)
+                {
+                    viewResult.View.RenderAsync(viewContext);
+                }
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+        public IActionResult RightClickShowOneButton(int buttonNumber)
+        {
+            // Right click reset button to zero state
+            buttons.ElementAt(buttonNumber).ButtonState = 0;
+
+            //return Json(package);
+
+            return PartialView("ShowOneButton", buttons.ElementAt(buttonNumber));
+        }
+    }
+}
---
+++ ButtonGrid/wwwroot/js/site.js
@@ -0,0 +1,59 @@
+﻿
+// Write your JavaScript code.
+$(function () {
+    //console.log("Page is loaded, yeah!");
+
+    /* $(document).on("click", ".game-button", function (event) {
+        event.preventDefault();
+
+        var buttonNumber = $(this).val();
+
+        console.log("Game button " + buttonNumber + " was clicked!");
+
+        doButtonUpdate(buttonNumber);
+    }); */
+
+    $(document).on("mousedown", ".game-button", function (event) {
+        event.preventDefault();
+        switch (event.which) {
+            case 1:
+                var buttonNumber = $(this).val();
+
+                doButtonUpdate(buttonNumber, '/Button/ShowOneButton');
+
+                //alert("Left mouse button clicked! Button number is: " + buttonNumber);
+                break;
+            case 2:
+                alert("Middle mouse button clicked!");
+                break;
+            case 3:
+                var buttonNumber = $(this).val();
+                doButtonUpdate(buttonNumber, '/Button/RightClickShowOneButton');
+                //alert("Right mouse button clicked! Button number is: " + buttonNumber);
+                break;
+            default:
+                alert("Nothing Clicked!");
+        }
+    });
+    $(document).bind("contextmenu", function (e) {
+        e.preventDefault();
+
+        console.log("The context menu is disabled");
+    })
+
+    function doButtonUpdate(buttonNumber, urlString) {
+        $.ajax({
+            method: 'POST',
+            url: urlString,
+            data: {
+                "buttonNumber": buttonNumber
+            },
+            success: function (data) {
+                //data should be a json with part1 and part2 objects in it, see button controller method SHOWONEBUTTON
+                console.log(data);
+                $("#" + buttonNumber).html(data.part1);
+                $(".messageArea").html(data.part2);
+            }
+        });
+    }
+});
